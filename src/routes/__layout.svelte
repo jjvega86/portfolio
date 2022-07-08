c64314317bc46eef36537971b9bf8e482cb37b30
JJ <jj.vega86@gmail.com>
2022-07-08 06:07:40 -0400

    Added layout to project

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<nav>
+	<a href="/">JJ Vega</a>
+	<a href="/about">About</a>
+	<a href="/projects">Projects</a>
+	<a href="/contact">Contact</a>
+</nav>
+
+<main>
+	<slot />
+</main>
+
+<footer>ICONS GO HERE</footer>
