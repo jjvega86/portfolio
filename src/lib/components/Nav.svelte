fdbf93718e2cdb31a1ec6acf5ff9b18340fad78a
JJ <jj.vega86@gmail.com>
2022-07-08 07:03:52 -0400

    Desktop navbar styling complete

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,12 +1,31 @@
 <nav>
-	<a href="/">JJ Vega</a>
-	<a href="/about">ABOUT</a>
-	<a href="/projects">PROJECTS</a>
-	<a href="/contact">CONTACT</a>
+	<div class="brand">
+		<a href="/">JJ Vega</a>
+	</div>
+	<ul class="links">
+		<li><a href="/projects">PROJECTS</a></li>
+		<li><a href="/resume">RESUME</a></li>
+		<li><a href="/about">ABOUT</a></li>
+		<li><a href="/contact">CONTACT ME</a></li>
+	</ul>
 </nav>
 
 <style>
 	nav {
 		display: flex;
+		justify-content: space-between;
+		padding: 1rem 2rem;
+		margin-bottom: 2rem;
+		box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+	}
+	nav a {
+		text-decoration: none;
+	}
+
+	.links {
+		display: flex;
+		justify-content: space-between;
+		list-style: none;
+		width: 50%;
 	}
 </style>
